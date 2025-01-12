9e0e0ca3b42b603b69f4ce5cbe245d035f02a1de
WisnuDwiSeisno <wseisno37@gmail.com>
2025-01-12 21:09:53 +0700

    membuat validasi error pada form input

--- app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/firebase/ui/viewmodel/InsertViewModel.kt
@@ -8,6 +8,20 @@ class InsertViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
 
 }
 
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val jenis_kelamin: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null,
+){
+    fun isValid() : Boolean {
+        return nim == null && nama == null && jenis_kelamin == null &&
+                alamat == null && kelas == null && angkatan == null
+    }
+}
+
 // Menyimpan input form kedalam entity
 fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
     nim = nim,
